b6279aa607aa8b3fb56fbf57c0ba8400cdaa99d7
Alexander Larsson <alexl@redhat.com>
2016-04-20 12:05:32 +0200

    Initial version of OCI support

--- app/xdg-app-builtins-build-bundle.c
+++ app/xdg-app-builtins-build-bundle.c
@@ -34,16 +34,23 @@
 #include "xdg-app-utils.h"
 #include "xdg-app-chain-input-stream.h"
 
+#ifdef HAVE_LIBARCHIVE
+#include <archive.h>
+#include <archive_entry.h>
+#endif
+
 static char *opt_arch;
 static char *opt_repo_url;
 static gboolean opt_runtime = FALSE;
 static char **opt_gpg_file;
+static gboolean opt_oci = FALSE;
 
 static GOptionEntry options[] = {
   { "runtime", 0, 0, G_OPTION_ARG_NONE, &opt_runtime, "Export runtime instead of app"},
   { "arch", 0, 0, G_OPTION_ARG_STRING, &opt_arch, "Arch to bundle for", "ARCH" },
   { "repo-url", 0, 0, G_OPTION_ARG_STRING, &opt_repo_url, "Url for repo", "URL" },
   { "gpg-keys", 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &opt_gpg_file, "Add GPG key from FILE (- for stdin)", "FILE" },
+  { "oci", 0, 0, G_OPTION_ARG_NONE, &opt_oci, "Export oci image instead of xdg-app bundle"},
 
   { NULL }
 };
@@ -244,6 +251,288 @@ build_bundle (OstreeRepo *repo, GFile *file,
   return TRUE;
 }
 
+#ifdef HAVE_LIBARCHIVE
+
+GLNX_DEFINE_CLEANUP_FUNCTION(void*, xdg_app_local_free_read_archive, archive_read_free)
+#define free_read_archive __attribute__ ((cleanup(xdg_app_local_free_read_archive)))
+
+GLNX_DEFINE_CLEANUP_FUNCTION(void*, xdg_app_local_free_write_archive, archive_write_free)
+#define free_write_archive __attribute__ ((cleanup(xdg_app_local_free_write_archive)))
+
+GLNX_DEFINE_CLEANUP_FUNCTION(void*, xdg_app_local_free_archive_entry, archive_entry_free)
+#define free_archive_entry __attribute__ ((cleanup(xdg_app_local_free_archive_entry)))
+
+static gboolean
+propagate_libarchive_error (GError      **error,
+                            struct archive *a)
+{
+  g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+               "%s", archive_error_string (a));
+  return FALSE;
+}
+
+struct archive_entry *
+new_entry (struct archive *a,
+           const char *name,
+           OstreeRepoExportArchiveOptions *opts)
+{
+  struct archive_entry *entry = archive_entry_new2 (a);
+  time_t ts = (time_t) opts->timestamp_secs;
+
+  archive_entry_update_pathname_utf8 (entry, name);
+  archive_entry_set_ctime (entry, ts, 0);
+  archive_entry_set_mtime (entry, ts, 0);
+  archive_entry_set_atime (entry, ts, 0);
+  archive_entry_set_uid (entry, 0);
+  archive_entry_set_gid (entry, 0);
+
+  return entry;
+}
+
+
+static gboolean
+add_dir (struct archive *a,
+         const char *name,
+         OstreeRepoExportArchiveOptions *opts,
+         GError **error)
+{
+  free_archive_entry struct archive_entry *entry = new_entry (a, name, opts);
+
+  archive_entry_set_mode (entry, AE_IFDIR | 0755);
+
+  if (archive_write_header (a, entry) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  return TRUE;
+}
+
+static gboolean
+add_symlink (struct archive *a,
+             const char *name,
+             const char *target,
+             OstreeRepoExportArchiveOptions *opts,
+             GError **error)
+{
+  free_archive_entry struct archive_entry *entry = new_entry (a, name, opts);
+
+  archive_entry_set_mode (entry, AE_IFLNK | 0755);
+  archive_entry_set_symlink (entry, target);
+
+  if (archive_write_header (a, entry) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  return TRUE;
+}
+
+static gboolean
+add_file (struct archive *a,
+          const char *name,
+          OstreeRepo *repo,
+          GFile *file,
+          OstreeRepoExportArchiveOptions *opts,
+          GCancellable *cancellable,
+          GError **error)
+{
+  free_archive_entry struct archive_entry *entry = new_entry (a, name, opts);
+  guint8 buf[8192];
+  g_autoptr(GInputStream) file_in = NULL;
+  g_autoptr(GFileInfo) file_info = NULL;
+  const char *checksum;
+
+  checksum = ostree_repo_file_get_checksum ((OstreeRepoFile*)file);
+
+  if (!ostree_repo_load_file (repo, checksum, &file_in, &file_info, NULL,
+                              cancellable, error))
+    return FALSE;
+
+  archive_entry_set_uid (entry, g_file_info_get_attribute_uint32 (file_info, "unix::uid"));
+  archive_entry_set_gid (entry, g_file_info_get_attribute_uint32 (file_info, "unix::gid"));
+  archive_entry_set_mode (entry, g_file_info_get_attribute_uint32 (file_info, "unix::mode"));
+  archive_entry_set_size (entry, g_file_info_get_size (file_info));
+
+  if (archive_write_header (a, entry) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  while (TRUE)
+    {
+      ssize_t r;
+      gssize bytes_read = g_input_stream_read (file_in, buf, sizeof (buf),
+                                               cancellable, error);
+      if (bytes_read < 0)
+        return FALSE;;
+      if (bytes_read == 0)
+        break;
+
+      r = archive_write_data (a, buf, bytes_read);
+      if (r != bytes_read)
+        return propagate_libarchive_error (error, a);
+    }
+
+  if (archive_write_finish_entry (a) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  return TRUE;
+}
+
+static gboolean
+add_file_from_data (struct archive *a,
+                    const char *name,
+                    OstreeRepo *repo,
+                    const char *data,
+                    gsize size,
+                    OstreeRepoExportArchiveOptions *opts,
+                    GCancellable *cancellable,
+                    GError **error)
+{
+  free_archive_entry struct archive_entry *entry = new_entry (a, name, opts);
+  ssize_t r;
+
+  archive_entry_set_mode (entry, AE_IFREG | 0755);
+  archive_entry_set_size (entry, size);
+
+  if (archive_write_header (a, entry) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  r = archive_write_data (a, data, size);
+  if (r != size)
+    return propagate_libarchive_error (error, a);
+
+  if (archive_write_finish_entry (a) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  return TRUE;
+}
+
+#endif
+
+static gboolean
+build_oci (OstreeRepo *repo, GFile *file,
+           const char *name, const char *full_branch,
+           GCancellable *cancellable, GError **error)
+{
+#ifdef HAVE_LIBARCHIVE
+  struct free_write_archive archive *a = NULL;
+  OstreeRepoExportArchiveOptions opts = { 0, };
+  g_autoptr(GFile) root = NULL;
+  g_autoptr(GFile) files = NULL;
+  g_autoptr(GFile) export = NULL;
+  g_autoptr(GFile) metadata = NULL;
+  g_autoptr(GVariant) commit_data = NULL;
+  g_autoptr(GVariant) commit_metadata = NULL;
+  g_autofree char *commit_checksum = NULL;
+
+  if (!ostree_repo_resolve_rev (repo, full_branch, FALSE, &commit_checksum, error))
+    return FALSE;
+
+  if (!ostree_repo_read_commit (repo, commit_checksum, &root, NULL, NULL, error))
+    return FALSE;
+
+  if (!ostree_repo_load_variant (repo, OSTREE_OBJECT_TYPE_COMMIT, commit_checksum, &commit_data, error))
+    return FALSE;
+
+  if (!ostree_repo_read_commit_detached_metadata (repo, commit_checksum, &commit_metadata, cancellable, error))
+    return FALSE;
+
+  a = archive_write_new ();
+
+  if (archive_write_set_format_gnutar (a) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  if (archive_write_add_filter_none (a) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  if (archive_write_open_filename (a, gs_file_get_path_cached (file)) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  opts.timestamp_secs = ostree_commit_get_timestamp (commit_data);
+
+  files = g_file_get_child (root, "files");
+  export = g_file_get_child (root, "export");
+  metadata = g_file_get_child (root, "metadata");
+
+  if (opt_runtime)
+    opts.path_prefix = "usr/";
+  else
+    opts.path_prefix = "app/";
+
+  {
+    const char *root_dirs[] = { "dev", "home", "proc", "run", "sys", "tmp", "var", "opt", "srv", "media", "mnt" };
+    const char *root_symlinks[] = {
+      "etc", "usr/etc",
+      "lib", "usr/lib",
+      "lib64", "usr/lib64",
+      "lib32", "usr/lib32",
+      "bin", "usr/bin",
+      "sbin", "usr/sbin",
+      "var/tmp", "/tmp",
+      "var/run", "/run",
+    };
+    int i;
+
+    /* Add the "other" of /app & /usr */
+    if (!add_dir (a, opt_runtime ? "app" : "usr", &opts, error))
+      return FALSE;
+
+    for (i = 0; i < G_N_ELEMENTS (root_dirs); i++)
+      if (!add_dir (a, root_dirs[i], &opts, error))
+        return FALSE;
+
+    for (i = 0; i < G_N_ELEMENTS (root_symlinks); i += 2)
+      if (!add_symlink (a, root_symlinks[i], root_symlinks[i+1], &opts, error))
+        return FALSE;
+  }
+
+  if (!ostree_repo_export_tree_to_archive (repo, &opts, (OstreeRepoFile*)files, a,
+                                           cancellable, error))
+    return FALSE;
+
+  if (!opt_runtime && g_file_query_exists (export, NULL))
+    {
+      opts.path_prefix = "export";
+      if (!ostree_repo_export_tree_to_archive (repo, &opts, (OstreeRepoFile*)export, a,
+                                               cancellable, error))
+        return FALSE;
+    }
+
+  opts.path_prefix = NULL;
+  if (!add_file (a, ".metadata", repo, metadata, &opts, cancellable, error))
+    return FALSE;
+
+  if (!add_file_from_data (a, ".ref",
+                           repo,
+                           full_branch,
+                           strlen (full_branch),
+                           &opts, cancellable, error))
+    return FALSE;
+
+  if (!add_file_from_data (a, ".commit",
+                           repo,
+                           g_variant_get_data (commit_data),
+                           g_variant_get_size (commit_data),
+                           &opts, cancellable, error))
+    return FALSE;
+
+  if (commit_metadata != NULL)
+    {
+      if (!add_file_from_data (a, ".commitmeta",
+                               repo,
+                               g_variant_get_data (commit_metadata),
+                               g_variant_get_size (commit_metadata),
+                               &opts, cancellable, error))
+        return FALSE;
+    }
+
+  if (archive_write_close (a) != ARCHIVE_OK)
+    return propagate_libarchive_error (error, a);
+
+  return TRUE;
+#else
+  g_set_error (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED,
+               "This version of xdg-app is not compiled with libarchive support");
+  return FALSE;
+#endif
+}
+
 gboolean
 xdg_app_builtin_build_bundle (int argc, char **argv, GCancellable *cancellable, GError **error)
 {
@@ -296,8 +585,16 @@ xdg_app_builtin_build_bundle (int argc, char **argv, GCancellable *cancellable, 
   if (!ostree_repo_open (repo, cancellable, error))
     return FALSE;
 
-  if (!build_bundle (repo, file, name, full_branch, cancellable, error))
-    return FALSE;
+  if (opt_oci)
+    {
+      if (!build_oci (repo, file, name, full_branch, cancellable, error))
+        return FALSE;
+    }
+  else
+    {
+      if (!build_bundle (repo, file, name, full_branch, cancellable, error))
+        return FALSE;
+    }
 
   return TRUE;
 }
